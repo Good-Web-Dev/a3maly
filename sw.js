38cf545ce6db5bcc92e5838b24cd19f9559b9922
GoodDeveloper <100660950+Good-Web-Dev@users.noreply.github.com>
2024-06-18 15:57:55 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,7 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('my-pwa-cache')
-      .then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/scripts.js',
-          '/icon-transition.css',
-          '/exams.html',
-          '/assignments.html',
-          '/researches.html',
-          '/projects.html',
-          '/icon.svg'
-        ]);
-      })
-  );
+  console.log('Service worker installed');
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+  console.log('Service worker fetch', event.request.url);
 });
